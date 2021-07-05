10a0ab26398142f8a989427af42f2cfa34ac5ec6
yuanjian-mike <yuanjian-mike@foxmail.com>
2021-07-05 21:55:51 +0800

    优化

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
-// import './styles/element-variables.scss'
+import './styles/element-variables.scss'
 import SuperFlow from 'vue-super-flow'
-// import './styles/vue-super-flow-variables.scss'
+import './styles/vue-super-flow-variables.scss'
 
 Vue.use(ElementUI)
 Vue.use(SuperFlow)
